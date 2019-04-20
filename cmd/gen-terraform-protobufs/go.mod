91b723bc7ecf051a2b82cdb34f29a62da0b80b7a
Bobby Powers <bobbypowers@gmail.com>
2019-04-20 12:27:22 -0700

    work on generating protobufs

--- cmd/gen-terraform-protobufs/go.mod
+++ cmd/gen-terraform-protobufs/go.mod
@@ -4,5 +4,7 @@ go 1.13
 
 require (
 	github.com/hashicorp/terraform v0.11.13
+	github.com/iancoleman/strcase v0.0.0-20180726023541-3605ed457bf7
+	github.com/sirupsen/logrus v1.4.1
 	github.com/terraform-providers/terraform-provider-aws v1.60.0
 )
--- cmd/gen-terraform-protobufs/main.go
+++ cmd/gen-terraform-protobufs/main.go
@@ -1,24 +1,123 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
+	"sort"
+	"text/template"
 
 	"github.com/hashicorp/terraform/helper/schema"
+	"github.com/iancoleman/strcase"
+	log "github.com/sirupsen/logrus"
 	"github.com/terraform-providers/terraform-provider-aws/aws"
 )
+
+// keys returns a sorted list of keys for a given schema map
+func keys(m map[string]*schema.Schema) []string {
+	keys := make([]string, 0, len(m))
+
+	for k := range m {
+		keys = append(keys, k)
+	}
+
+	sort.Strings(keys)
+
+	return keys
+}
+
+type fieldDef struct {
+	Name        string
+	Number      uint
+	Type        schema.ValueType
+	Optional    bool
+	ElementType *schema.ValueType
+	MinItems    *uint
+	MaxItems    *uint
+}
+
+type messageDef struct {
+	Name string
+	// messages can have sub-messages, referenced by fields
+	Messages []*messageDef
+	Fields   []*fieldDef
+}
+
+func newMessage(name string, resource *schema.Resource) (*messageDef, error) {
+	msg := &messageDef{
+		Name:   strcase.ToCamel(name),
+		Fields: make([]*fieldDef, 0, len(resource.Schema)),
+	}
+
+	fieldNames := keys(resource.Schema)
+	for i, name := range fieldNames {
+		s := resource.Schema[name]
+
+		def := &fieldDef{
+			Name:     name,
+			Number:   uint(i),
+			Type:     s.Type,
+			Optional: s.Optional,
+		}
+
+		// first check for nil interface value (different from
+		// a typed-nil in the interface)
+		if elem := s.Elem; elem != nil {
+			switch e := elem.(type) {
+			case *schema.Resource:
+				if e == nil {
+					break
+				}
+				log.Infof("TODO: field %s elem resource: %v", name, e)
+			case *schema.Schema:
+				if e == nil {
+					break
+				}
+				ty := e.Type
+				def.ElementType = &ty
+			}
+		}
+		// also schema.Elem + others. see:
+		// https://github.com/terraform-providers/terraform-provider-aws/blob/master/aws/data_source_aws_lb.go#L57
+
+		msg.Fields = append(msg.Fields, def)
+	}
+
+	return msg, nil
+}
+
+var messageTemplate = template.Must(template.New("message").Parse(`message {{.Name}} {
+  
+}`))
+
+func (msg *messageDef) String() string {
+	var buf bytes.Buffer
+	err := messageTemplate.Execute(&buf, msg)
+	if err != nil {
+		panic("template execution failed!")
+	}
+
+	return buf.String()
+}
 
 func main() {
 	tfProvider := aws.Provider()
 	provider := tfProvider.(*schema.Provider)
 
 	for name, resource := range provider.ResourcesMap {
-		if name == "aws_lb" {
-			fmt.Printf("%s: %v\n", name, resource.SchemaVersion)
-			for field, schema := range resource.Schema {
-				fmt.Printf("\t%s: %v (%v)\n", field, schema.Type, schema.Optional)
-				// also schema.Elem + others. see:
-				// https://github.com/terraform-providers/terraform-provider-aws/blob/master/aws/data_source_aws_lb.go#L57
-			}
+		if name != "aws_lb" {
+			continue
 		}
+
+		if resource.Schema == nil {
+			log.Warnf("resource %s missing schema", name)
+			continue
+		}
+
+		msg, err := newMessage(name, resource)
+		if err != nil {
+			log.Errorf("newMessage(%s): %s", name, err)
+		}
+
+		fmt.Printf("MSG: %s\n", msg)
 	}
 }
